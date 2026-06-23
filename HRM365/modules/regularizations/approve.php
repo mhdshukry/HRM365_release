113e4d519922709972d5f739cc7960b19202e2c0
Fathima Athiyya <mhdshukry110@gmail.com>
2026-06-23 14:10:15 +0530

    Update approve.php

--- HRM365/modules/regularizations/approve.php
+++ HRM365/modules/regularizations/approve.php
@@ -100,7 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         $raw_ot_hours = round($ot_seconds / 3600, 2);
                         if ($raw_ot_hours > 0.5) {
                             $overtime_hours = $raw_ot_hours;
-                            $rate = floatval($emp['overtime_rate_per_hour']) > 0 ? floatval($emp['overtime_rate_per_hour']) : 1.0;
+                            $rate = floatval($emp['overtime_rate_per_hour']) > 0 ? floatval($emp['overtime_rate_per_hour']) : PAYROLL_NORMAL_OT_RATE;
                             $overtime_amount = calculate_overtime_amount(
                                 $overtime_hours,
                                 floatval($emp['base_salary']),
@@ -118,7 +118,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $updateRec = $pdo->prepare("
                 UPDATE attendance_records 
                 SET shift_id = ?, attendance_policy_id = ?, clock_in = ?, clock_out = ?, total_hours = ?, 
-                    is_late = ?, is_early_departure = ?, 
+                    is_late = ?, is_early_departure = ?, is_absent = 0,
                     overtime_hours = ?, overtime_amount = ?, is_holiday = ?, is_weekend = ?,
                     status = 'Present'
                 WHERE id = ?
